f6a67bf453157bc00f491fcdec6c13977981aa3d
Eka Darmawan <eka_darma@hotmail.com>
2022-07-19 23:23:00 -0400

    Studio Area Circle

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 public class Area {
     public static void main (String[] args) {
-        Double pi = 3.14;
         Scanner input = new Scanner (System.in);
         System.out.println("Enter a Radius: ");
-        Double r = input.nextDouble();
+        double r = input.nextDouble();
 
         if (r < 0) {
             System.out.println("Radius must be Positive Number");
         } else {
             // Double A = pi * r * r;
-            Double A = Circle.getArea(r); // Calling the Circle.java
+            double A = Circle.getArea(r); // Calling the Circle.java
             System.out.println("The area of a circle of radius " + r + " is: " + A);
         }
     }
